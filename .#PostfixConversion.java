50c3fafa64e43c80c24ac95a92eea61b001a79f5
Owen Na <osn53387@uga.edu>
2023-02-24 21:50:08 -0500

    modified LinkedListStack

--- .#PostfixConversion.java
+++
@@ -1,1 +0,0 @@
-osn53387@csci-odin.cs.uga.edu.5157:1638798037
--- PostfixConversion.java
+++ PostfixConversion.java
@@ -3,7 +3,18 @@ public class PostfixConversion() {
     private boolean checker;
 
     public boolean validate(LinkedListStack stack) {
+        LinkedListStack stack1 = new LinkedListStack();
+        while (stack.top() != null) {
+            if (stack.top() = "(") {
+                stack1.push(stack.pop());
+            } else if (stack.top() != "(") {
+                if (stack1.top(stack.pop())) {
 
+                } // if
+
+
+            } // else if
+        } // while
     } // validate
 
     public LinkedListStack convert(LinkedListStack stack) {
